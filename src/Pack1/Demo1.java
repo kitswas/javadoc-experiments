7a7694546ef046cbac38ce9e1cb04c51dd4d6218
kitswas <90329875+kitswas@users.noreply.github.com>
2022-05-02 22:34:47 +0530

    Created files and folders

---
+++ src/Pack1/Demo1.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+public class Demo1 {
+    private static void some_private_method() {
+        // Does nothing
+    }
+
+    public void tell_the_world() {
+        System.out.println("I know not why I am doing this!");
+    }
+}
---
+++ src/Pack2/Demo2.java
@@ -0,0 +1,11 @@
+package Pack2;
+
+public class Demo2 {
+    protected void foo() {
+        // Waste of time and space
+    }
+
+    static double bar(int x) {
+        return x;
+    }
+}
